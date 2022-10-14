35cc5c512a81e847daa5a377c99f69b313561596
lianzi liu <lianziliu.sugar@gmail.com>
2022-10-14 11:28:44 -0400

    finish minus and plus btn

--- js/challenge.js
+++ js/challenge.js
@@ -1,1 +1,24 @@
-console.log ("Lianzi part")
+const countNum = document.querySelector("#counter");
+const list = document.querySelector("#list");
+
+const minusBtn = document.querySelector("#minus");
+minusBtn.addEventListener("click", function(){
+
+    countNum.textContent = parseInt(countNum.textContent) - 1;
+})
+
+
+const plusBtn = document.querySelector("#plus");
+plusBtn.addEventListener("click", function(){
+
+    countNum.textContent = parseInt(countNum.textContent) + 1;
+
+})
+
+const heartBtn = document.querySelector("#heart");
+heartBtn.addEventListener("click", function(){
+
+   const li =  document.createElement
+
+
+})
